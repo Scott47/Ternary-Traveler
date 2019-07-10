ceed36a3d5b26359f1121d0c55b34ca48c99c12e
scottsilver <scottsilver47@gmail.com>
2019-07-10 10:33:39 -0500

    created from for adding new point of interest

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -6,6 +6,11 @@ function welcomeTravelerPage (){
     <div id="welcome-traveler">
     <h1>Welcome Mira!</h1>
     <h2>Here are your points of interest</h2>
+    <h5>Italy</h5>
+    <hr>
+    <h5>Switzerland</h5>
+    <hr>
+    <h5>France</h5>
     <button id="new-interest">Add New Point of Interest</button>
     </div>
     `
@@ -18,15 +23,15 @@ function createNewInterestForm() {
             <input id="interest-name" type="text" name="interest-name" required>
             <label for="cost"><h4>Cost: </h4></label>
             <input id="cost" type="type" name="cost" required>
-            <label for="event-time"><h4>Time: </h4></label>
-            <input id="event-time" type="time" name="event-time" required>
-            <label for="event-location"><h4>Location: </h4></label>
-            <input id="event-location" type="text" name="event-location" required>
-            <button id="save-event">Save Event</button>
+            <label for="place"><h4>Location: </h4></label>
+            <select id="location-drop" name="location-drop">
+                <option value="Italy">Italy</option>
+                <option value="Switzerland">Switzerland</option>
+                <option value="France">France</option>
+            </select>
+            </label>
+            <button id="save-interest">Save interest</button>
         </fieldset>
         `;
-//         1. Name of the point of interest
-// 1. Description of the point of interest
-// 1. Cost of visiting the point of interest
-// 1. Dropdown to pick which place the point of interest is located in
+}
 export { welcomeTravelerPage }
